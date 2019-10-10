c045dd8b2f0df5352fd36121a499dd41869135d1
Nikolay Toporkov <toporkov.kolya96@gmail.com>
2019-10-10 17:35:27 +0300

    disable check maturity

--- ducatuscore/src/main.cpp
+++ ducatuscore/src/main.cpp
@@ -2049,13 +2049,13 @@ bool CheckTxInputs(const CTransaction& tx, CValidationState& state, const CCoins
             assert(coins);
 
             // If prev is coinbase, check that it's matured
-            if (coins->IsCoinBase()) {
-                if (nSpendHeight - coins->nHeight < COINBASE_MATURITY)
-				            strprintf("Maturity: %d", COINBASE_MATURITY);
-                    return state.Invalid(false,
-                        REJECT_INVALID, "bad-txns-premature-spend-of-coinbase",
-                        strprintf("tried to spend coinbase at depth %d", nSpendHeight - coins->nHeight));
-            }
+//            if (coins->IsCoinBase()) {
+//                if (nSpendHeight - coins->nHeight < COINBASE_MATURITY)
+//				            strprintf("Maturity: %d", COINBASE_MATURITY);
+//                    return state.Invalid(false,
+//                        REJECT_INVALID, "bad-txns-premature-spend-of-coinbase",
+//                        strprintf("tried to spend coinbase at depth %d", nSpendHeight - coins->nHeight));
+//            }
 
             // Check for negative or overflow input values
             nValueIn += coins->vout[prevout.n].nValue;
