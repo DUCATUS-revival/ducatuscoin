31acbd78c44142bd46d72235973187a71f0a9702
Mikhail Rozdin <mrozdin@yandex.ru>
2019-10-14 15:43:47 +0300

    rename DTC to DUC

--- fnr.sh
+++ fnr.sh
@@ -3,7 +3,7 @@
 set -x
 
 COIN_LONG_NAME="ducatuscoin"
-COIN_ABBREVIATION="DTC"
+COIN_ABBREVIATION="DUC"
 
 sed -i "s/dctcoin/$COIN_LONG_NAME/g" README.md autogen.sh src/*.h src/*.cpp configure.ac
 sed -i "s/DTC/$COIN_ABBREVIATION/g" README.md autogen.sh src/*.h src/*.cpp configure.ac
