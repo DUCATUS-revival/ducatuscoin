0f70c7d55e3131bcb31f934445ad45df996d0d34
Euphemia Deathridge <ephdtrg@mintyclouds.in>
2021-11-17 22:44:19 +0300

    unlock mining wallet with passhprase

--- automine.sh
+++ automine.sh
@@ -3,10 +3,21 @@
 rpcport=$(cat /root/.ducatuscoin/ducatuscoin.conf | grep rpcport | cut -c 9-  )
 rpcuser=$(cat /root/.ducatuscoin/ducatuscoin.conf | grep rpcuser | cut -c 9-  )
 rpcpassword=$(cat /root/.ducatuscoin/ducatuscoin.conf | grep rpcpassword | cut -c 13-  )
+walletpassphrase=$(head -1 /root/.node_pwd | grep rpcpassword | cut -c 13-  )
 
 # echo "$rpcport $rpcuser $rpcpassword"
 
 while true; do
+    echo "Unlock wallet with passphrase"
+    ducatuscoin-cli \
+      -testnet \
+      -rpcconnect=ducatuscoin_node \
+      -rpcport=$rpcport \
+      -rpcuser=$rpcuser \
+      -rpcpassword=$rpcpassword \
+      walletpassphrase $walletpassphrase 1000000
+    
+    echo "Refill keypool"
     ducatuscoin-cli \
       -testnet \
       -rpcconnect=ducatuscoin_node \
@@ -15,6 +26,7 @@ while true; do
       -rpcpassword=$rpcpassword \
       keypoolrefill 1
 
+    echo "Generate blocks"
     ducatuscoin-cli \
       -testnet \
       -rpcconnect=ducatuscoin_node \
